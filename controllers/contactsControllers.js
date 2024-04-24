91a31db0f6d5b16134e96abdf5c52e43898f18dd
Myronenko <55333290+SergeyMyronenko@users.noreply.github.com>
2024-04-24 12:50:51 +0300

    add controlers

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,1 +1,41 @@
+import {
+  addContact,
+  getContactById,
+  listContacts,
+  removeContact,
+} from "../services/contactsServices.js";
 
+export async function getAllContacts(req, res) {
+  const allContacts = await listContacts();
+  res.status(200).json(allContacts);
+}
+
+export async function getOneContact(req, res) {
+  const { id } = req.params;
+  const oneContact = getContactById(id);
+  if (oneContact) {
+    res.status(200).json(oneContact);
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
+}
+
+export async function deletedContactById(req, res) {
+  const { id } = req.params;
+  const deletedContact = removeContact(id);
+  if (deletedContact) {
+    res.status(200).json(deletedContact);
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
+}
+
+export async function AddNewContact(req, res) {
+  const { name, email, phone } = req.params;
+  const newContact = addContact(name, email, phone);
+  if (!name || !email || !phone) {
+    res.status(400).json({ message: "Filds must be filled" });
+  } else {
+    res.status(201).json(newContact);
+  }
+}
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,32 +1,21 @@
 import express from "express";
+
 import {
-  addContact,
-  getContactById,
-  listContacts,
-  removeContact,
-} from "../services/contactsServices.js";
+  AddNewContact,
+  deletedContactById,
+  getAllContacts,
+  getOneContact,
+} from "../controllers/contactsControllers.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const allContacts = await listContacts();
-  res.json(allContacts);
-});
+router.get("/", getAllContacts);
 
-router.get("/:id", (req, res) => {
-  const oneContact = getContactById();
-  res.json(oneContact);
-});
+router.get("/:id", getOneContact);
 
-router.delete("/:id", (req, res) => {
-  const deletedContact = removeContact();
-  res.json(deletedContact);
-});
+router.delete("/:id", deletedContactById);
 
-router.post("/", (req, res) => {
-  const addContact = addContact();
-  res.json(addContact);
-});
+router.post("/", AddNewContact);
 
 // router.put("/api/contacts/:id", (req, res) => {});
 
